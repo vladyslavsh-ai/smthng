62ceab74bb98dea27a8158e757bba29b71673cdf
vladyslavsh-ai <vladyslav.sh@gameup.club>
2025-12-01 16:35:36 +0200

    Update cat-christmas-calendar-script.js

--- cat-christmas-calendar-script.js
+++ cat-christmas-calendar-script.js
@@ -131,7 +131,7 @@
     });
   }); 
   const initializeCalendarDays = () => {
-    const startDate = new Date('2025-11-01');
+    const startDate = new Date('2025-12-01');
     const currentDate = new Date();
     let timeDiff = currentDate - startDate;
     let originalDaysPassed = timeDiff >= 0 ? Math.floor(timeDiff / (1000 * 3600 * 24)) + 1 : 0; 
@@ -240,4 +240,5 @@
     initializeCalendarDays();
     initializeAccordions();
   }
+
 })();
